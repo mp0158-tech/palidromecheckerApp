867ba96066fbe85f433cfba2a1b75c5069171ad8
mp0158-tech <mp0158@srmist.edu.in>
2026-02-26 10:14:16 +0530

    [manan patel] added [UC1]

---
+++ app/src/UC1.java
@@ -0,0 +1,21 @@
+public class UC1 {
+
+    public static void main(String[] args) {
+
+        // Display Application Name
+        System.out.println("=================================");
+        System.out.println("      Palindrome Checker App     ");
+        System.out.println("=================================");
+
+        // Display Use Case
+        System.out.println("UC1: Application Entry & Welcome Message");
+
+        // Display Version
+        System.out.println("Version: 1.0");
+
+        System.out.println("---------------------------------");
+        System.out.println("Application Started Successfully!");
+        System.out.println("---------------------------------");
+
+    }
+}
